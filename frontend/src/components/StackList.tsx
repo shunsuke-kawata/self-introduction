749fb3c761fd1ce0e8706540ab37793ab9ac473d
shunsuke-kawata <kwt20238120@gmail.com>
2024-07-18 02:24:20 +0900

    update プログラミング言語の表示部分を作成

--- frontend/src/components/StackList.tsx
+++ frontend/src/components/StackList.tsx
@@ -2,22 +2,29 @@ import config from "../config/config.json";
 const StackList = () => {
   return (
     <>
-      {config.main_stacks.programming_languages.map(
-        (programming_language, index) => (
-          <div key={index}>
-            <div className="img-and-name">
-              <img
-                className="stack-icon-img"
-                src={programming_language.icon_path}
-                alt={programming_language.alt_string}
-              />
-              <label className="language-name-label">
-                {programming_language.language}
-              </label>
+      <p className="stacks-division-title">プログラミング言語</p>
+      <div className="stack-container">
+        {config.main_stacks.programming_languages.map(
+          (programming_language, index) => (
+            <div key={index} className="stack-info-div">
+              <div className="img-and-name">
+                <img
+                  className="stack-icon-img"
+                  src={programming_language.icon_path}
+                  alt={programming_language.alt_string}
+                />
+                <label className="language-name-label">
+                  {programming_language.language}
+                </label>
+              </div>
+              <div className="language-desc">
+                <label>{programming_language.description}</label>
+              </div>
             </div>
-          </div>
-        )
-      )}
+          )
+        )}
+      </div>
+      <p className="stacks-division-title">フレームワークなど</p>
     </>
   );
 };
