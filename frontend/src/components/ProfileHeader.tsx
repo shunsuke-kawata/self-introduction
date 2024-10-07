28849782e70173eb964aaab12a1b5e606fcbf4f3
shunsuke-kawata <kwt20238120@gmail.com>
2024-10-08 00:01:00 +0900

    fix　レスポンシブ対応

--- frontend/src/components/ProfileHeader.tsx
+++ frontend/src/components/ProfileHeader.tsx
@@ -1,7 +1,7 @@
 import "../styles/Components.css";
 import config from "../config/config.json";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProfileModal from "./ProfileModal";
 
 const ProfileHeader: React.FC = () => {
@@ -9,6 +9,28 @@ const ProfileHeader: React.FC = () => {
   const openProfileModal = () => {
     setIsOpenProfileModal(true);
   };
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // ウィンドウサイズの変更時にサイズを更新する関数
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // リスナーを追加
+    window.addEventListener("resize", handleResize);
+
+    // クリーンアップ時にリスナーを削除
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <div className="profile-header-main">
@@ -20,24 +42,52 @@ const ProfileHeader: React.FC = () => {
         <div className="profile-header-info">
           <p className="profile-name">{config.main_profile.name}</p>
           <p className="main-profile-info">
-            @{config.main_profile.name_en}・{config.main_profile.name_kana}
+            @{config.main_profile.name_en}
+            {windowSize.width >= 610
+              ? "・" + config.main_profile.name_kana
+              : ""}
           </p>
-          <p className="main-profile-info">
-            名前：{config.main_profile.name}
-            <label
-              className="more-info-label"
-              onClick={() => openProfileModal()}
-            >
-              ...さらに表示
-            </label>
-          </p>
+          {windowSize.width >= 768 ? (
+            <>
+              <p className="main-profile-info">
+                名前：{config.main_profile.name}
+              </p>
+              <p className="main-profile-info">
+                誕生日：{config.main_profile.birthday}
+              </p>
+              <p className="main-profile-info">
+                出身：{config.main_profile.birthplace}
+              </p>
+              <p className="main-profile-info">
+                所属：{config.main_profile.majoring}
+              </p>
+              <p className="main-profile-info">
+                趣味：{config.main_profile.hobbies}
+              </p>
+            </>
+          ) : (
+            <>
+              <p className="main-profile-info">
+                名前：{config.main_profile.name}
+                <label
+                  className="more-info-label"
+                  onClick={() => openProfileModal()}
+                >
+                  ...さらに表示
+                </label>
+              </p>
+            </>
+          )}
+
           <a
             className="main-profile-links"
             href={config.main_profile.links[0].url}
             target="_blank"
             rel="noreferrer"
           >
-            {config.main_profile.links[0].url}
+            {windowSize.width >= 700
+              ? config.main_profile.links[0].url
+              : config.main_profile.links[0].url.substring(0, 15) + "...."}
           </a>
         </div>
       </div>
